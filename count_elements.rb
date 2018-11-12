ebb67913b2070e85fb7aa1ff59a2b02e3f611512
a lan <bluenowte97@gmail.com>
2018-11-12 21:34:45 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -8,7 +8,7 @@ def count_elements(array)
       if hash[element] = nil # if not in hash, then assign it
        hash[element] = 1
        count = 1
-      
+
     elsif hash[element] != nil
       count = count + 1
       hash[element] = count
