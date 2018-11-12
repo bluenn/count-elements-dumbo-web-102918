ce81a396bc813f1749bee8450acbb75a7243141b
a lan <bluenowte97@gmail.com>
2018-11-12 21:43:55 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,7 +2,7 @@ def count_elements(array)
   # code goes here
   count = 0
   hash = {}
-  array.map do |element|
+  array.select do |element|
     if hash.has_key?(element) != true # if not in hash, then assign it
        hash[element] = 1
        count = 1
