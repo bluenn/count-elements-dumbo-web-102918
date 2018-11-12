a7affeda1b41f45a023fd235e70d736b9920b520
a lan <bluenowte97@gmail.com>
2018-11-12 21:35:13 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -4,8 +4,7 @@ def count_elements(array)
   word = ""
   hash = {}
   array.each do |element|
-
-      if hash[element] == nil # if not in hash, then assign it
+    if hash[element] == nil # if not in hash, then assign it
        hash[element] = 1
        count = 1
 
