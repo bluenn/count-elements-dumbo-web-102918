ea4bd2da2e3a7ce42538c041642609013ad44707
a lan <bluenowte97@gmail.com>
2018-11-12 21:54:40 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,16 +2,19 @@ def count_elements(array)
   # code goes here
   count = 0
   hash = {}
-  array.map do |element|
-    if hash.has_key?(element) != true # if not in hash, then assign it
-       hash[element] = 1
-       count = 1
-
-    elsif hash.has_key?(element) == true             #if in hash, then just increase count
-      count+=  1
-      hash[element] = count
-    end
-  end
+  array.uniq.each do |element|
+  hash[element] = array.count(element)
+end
+  # array.map do |element|
+  #   if hash.has_key?(element) != true # if not in hash, then assign it
+  #      hash[element] = 1
+  #      count = 1
+  # 
+  #   elsif hash.has_key?(element) == true             #if in hash, then just increase count
+  #     count+=  1
+  #     hash[element] = count
+  #   end
+  # end
   puts hash
   hash
 end
